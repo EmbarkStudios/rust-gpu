0ba2d6deb11556c4b1b054fdc5861ff8419bb0e3
Jasper Bekkers <bekkers@gmail.com>
2020-07-24 12:30:32 +0100

    Add Copy & int literal to test

--- tests/empty.rs
+++ tests/empty.rs
@@ -10,5 +10,26 @@ pub trait Unsize<T: ?Sized> {}
 #[lang = "coerce_unsized"]
 pub trait CoerceUnsized<T> {}
 
+#[lang = "copy"]
+pub unsafe trait Copy {}
 
-pub fn jasper() {}
+unsafe impl Copy for bool {}
+unsafe impl Copy for u8 {}
+unsafe impl Copy for u16 {}
+unsafe impl Copy for u32 {}
+unsafe impl Copy for u64 {}
+unsafe impl Copy for usize {}
+unsafe impl Copy for i8 {}
+unsafe impl Copy for i16 {}
+unsafe impl Copy for i32 {}
+unsafe impl Copy for isize {}
+unsafe impl Copy for f32 {}
+unsafe impl Copy for char {}
+unsafe impl<'a, T: ?Sized> Copy for &'a T {}
+unsafe impl<T: ?Sized> Copy for *const T {}
+unsafe impl<T: ?Sized> Copy for *mut T {}
+
+
+pub fn jasper() {
+    let _ktest = 23;
+}
