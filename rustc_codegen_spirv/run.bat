e31ea83852023749ea55db35a19d8de2df931cc2
khyperia <github@khyperia.com>
2020-09-01 16:44:51 +0200

    Delete old scripts no longer used

--- rustc_codegen_spirv/run.bat
+++
@@ -1,1 +0,0 @@
-rustc -Zcodegen-backend=target\debug\rustc_codegen_spirv.dll --crate-name=empty --crate-type lib test\empty.rs
--- rustc_codegen_spirv/run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-rustc -Zcodegen-backend=target/debug/librustc_codegen_spirv.so --crate-type lib test/empty.rs
