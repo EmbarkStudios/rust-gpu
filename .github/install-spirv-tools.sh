7fbb42778ec6dd780c6bfca6875a071b7baed75b
Jake Shadle <jake.shadle@embark-studios.com>
2020-09-29 15:30:55 +0200

    Use HOME

--- .github/install-spirv-tools.sh
+++ .github/install-spirv-tools.sh
@@ -5,16 +5,16 @@ os="$1"
 
 tmparch=$(mktemp)
 
-mkdir ~/spirv-tools
+mkdir "${HOME}/spirv-tools"
 
 if [ "$os" == 'Linux' ]; then
     curl -fL -o "$tmparch" https://storage.googleapis.com/spirv-tools/artifacts/prod/graphics_shader_compiler/spirv-tools/linux-clang-release/continuous/1250/20200928-083216/install.tgz
 
-    tar xzf "$tmparch" -C ~/spirv-tools
+    tar xzf "$tmparch" -C "${HOME}/spirv-tools"
 elif [ "$os" == 'Windows' ]; then
     curl -fL -o "$tmparch" https://storage.googleapis.com/spirv-tools/artifacts/prod/graphics_shader_compiler/spirv-tools/windows-msvc-2017-release/continuous/1232/20200928-085551/install.zip
 
-    unzip "$tmparch" -d ~/spirv-tools
+    unzip "$tmparch" -d "${HOME}/spirv-tools"
 else
     echo "Unimplemented host os $os"
     exit 1
