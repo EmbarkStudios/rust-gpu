d183e6f304bfeee2efea16577c553ebf01e39d34
Jake Shadle <jake.shadle@embark-studios.com>
2020-09-29 16:09:54 +0200

    WAT

--- rspirv-linker/src/test.rs
+++ rspirv-linker/src/test.rs
@@ -23,6 +23,8 @@ fn assemble_spirv(spirv: &str) -> Vec<u8> {
 
     std::fs::write(&input, spirv).unwrap();
 
+    println!("WAT {}", std::env::var("PATH").unwrap());
+
     let process = Command::new("spirv-as")
         .arg(input.to_str().unwrap())
         .arg("-o")
