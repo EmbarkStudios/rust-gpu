60a4164ca1b76a73605b2d548ee7989ebdbb8553
khyperia <github@khyperia.com>
2020-10-09 20:01:35 +0200

    Reference rustc_codegen_spirv in spirv-builder
    This makes the UX much better. It's also kind of bonkers, but, in a good
    way. I think.

--- examples/example-runner/build.rs
+++ examples/example-runner/build.rs
@@ -3,6 +3,6 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // This will set the env var `example-shader.spv` to a spir-v file that can be include!()'d
-    build_spirv("../../rustc_codegen_spirv", "../example-shader")?;
+    build_spirv("../example-shader")?;
     Ok(())
 }
--- spirv-builder/src/lib.rs
+++ spirv-builder/src/lib.rs
@@ -25,12 +25,8 @@ impl fmt::Display for SpirvBuilderError {
 
 impl Error for SpirvBuilderError {}
 
-pub fn build_spirv(
-    path_to_rustc_codegen_spirv: impl AsRef<Path>,
-    path_to_crate: impl AsRef<Path>,
-) -> Result<(), SpirvBuilderError> {
-    let path_to_built_codegen = build_rustc_codegen_spirv(path_to_rustc_codegen_spirv.as_ref())?;
-    let spirv_module = invoke_rustc(path_to_built_codegen, path_to_crate.as_ref())?;
+pub fn build_spirv(path_to_crate: impl AsRef<Path>) -> Result<(), SpirvBuilderError> {
+    let spirv_module = invoke_rustc(path_to_crate.as_ref())?;
     let spirv_path: &Path = spirv_module.as_ref();
     let env_var = spirv_path.file_name().unwrap().to_str().unwrap();
     println!("cargo:rustc-env={}={}", env_var, spirv_module);
@@ -65,30 +61,18 @@ fn get_last_artifact(out: &str) -> String {
     }
 }
 
-fn build_rustc_codegen_spirv(path_to_codegen: &Path) -> Result<String, SpirvBuilderError> {
-    let build = Command::new("cargo")
-        .args(&[
-            "build",
-            "--message-format=json-render-diagnostics",
-            "--release",
-        ])
-        .current_dir(path_to_codegen)
-        .output()
-        .expect("failed to execute cargo build");
-    if build.status.success() {
-        Ok(get_last_artifact(&String::from_utf8(build.stdout).unwrap()))
-    } else {
-        Err(SpirvBuilderError::BuildFailed(build))
-    }
-}
-
-fn invoke_rustc(
-    path_to_built_codegen: String,
-    path_to_crate: &Path,
-) -> Result<String, SpirvBuilderError> {
+fn invoke_rustc(path_to_crate: &Path) -> Result<String, SpirvBuilderError> {
+    // Okay, this is a little bonkers: in a normal world, we'd have the user clone
+    // rustc_codegen_spirv and pass in the path to it, and then we'd invoke cargo to build it, grab
+    // the resulting .so, and pass it into -Z codegen-backend. But that's really gross: the user
+    // needs to clone rustc_codegen_spirv and tell us its path! So instead, we *directly reference
+    // rustc_codegen_spirv in spirv-builder's Cargo.toml*, which means that it will get built
+    // alongside build.rs, and cargo will helpfully add it to LD_LIBRARY_PATH for us! So we just
+    // need to pass the direct filename here.
     let rustflags = format!(
-        "-Z codegen-backend={} -C target-feature=+shader",
-        path_to_built_codegen
+        "-Z codegen-backend={}rustc_codegen_spirv{} -C target-feature=+shader",
+        std::env::consts::DLL_PREFIX,
+        std::env::consts::DLL_SUFFIX
     );
     let build = Command::new("cargo")
         .args(&[
