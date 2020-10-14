b69ff3f4c63481cc9ceaef3b94a79754134eb1a1
khyperia <github@khyperia.com>
2020-10-14 15:09:36 +0200

    Move to a builder struct for spirv-builder
    This allows the print_metadata option

--- examples/example-runner/build.rs
+++ examples/example-runner/build.rs
@@ -1,8 +1,8 @@
-use spirv_builder::build_spirv;
+use spirv_builder::SpirvBuilder;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // This will set the env var `example-shader.spv` to a spir-v file that can be include!()'d
-    build_spirv("../example-shader")?;
+    SpirvBuilder::new("../example-shader").build()?;
     Ok(())
 }
