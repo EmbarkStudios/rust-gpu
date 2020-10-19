782faa6cf4de4c5af3283000060c866dffc88d87
Jasper Bekkers <bekkers@gmail.com>
2020-10-19 21:12:45 +0200

    Branchless sky shader

--- rustc_codegen_spirv/src/builder/builder_methods.rs
+++ rustc_codegen_spirv/src/builder/builder_methods.rs
@@ -1375,7 +1375,7 @@ impl<'a, 'tcx> BuilderMethods<'a, 'tcx> for Builder<'a, 'tcx> {
             .copy_memory_sized(dst.def, src.def, size.def, None, None, empty())
             .unwrap();
         if !self.builder.has_capability(Capability::Addresses) {
-            self.zombie(dst.def, "OpCopyMemorySized without OpCapability Addresses");
+            self.zombie(dst.def, "OpCopyMemorySized (memcpy) without OpCapability Addresses");
         }
     }
 
@@ -1392,7 +1392,7 @@ impl<'a, 'tcx> BuilderMethods<'a, 'tcx> for Builder<'a, 'tcx> {
             .copy_memory_sized(dst.def, src.def, size.def, None, None, empty())
             .unwrap();
         if !self.builder.has_capability(Capability::Addresses) {
-            self.zombie(dst.def, "OpCopyMemorySized without OpCapability Addresses");
+            self.zombie(dst.def, "OpCopyMemorySized (memmov) without OpCapability Addresses");
         }
     }
 
