cfd3c5c7f9012f42182f6e92ea1b83d33e704d4d
VZout <viktor.zoutman@embark-studios.com>
2020-10-19 17:31:41 +0200

    The example now also uses the vertex shader

--- examples/example-shader/src/lib.rs
+++ examples/example-shader/src/lib.rs
@@ -8,7 +8,19 @@ use spirv_std::{f32x4, Input, Output};
 #[allow(unused_attributes)]
 #[spirv(entry = "fragment")]
 pub fn main(input: Input<f32x4>, mut output: Output<f32x4>) {
-    output.store(input.load())
+    output.store(input.load());
+}
+
+#[allow(unused_attributes)]
+#[spirv(entry = "vertex")]
+pub fn main_vs(
+    in_pos: Input<f32x4>,
+    in_color: Input<f32x4>,
+    #[spirv(builtin = "position")] mut out_pos: Output<f32x4>,
+    mut out_color: Output<f32x4>,
+) {
+    out_pos.store(in_pos.load());
+    out_color.store(in_color.load());
 }
 
 #[panic_handler]
