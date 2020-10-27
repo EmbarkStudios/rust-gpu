5b538ee5eea141ddeb652beca42d0001423ccfba
Ashley Hauck <github@khyperia.com>
2020-10-27 14:16:40 +0100

    Implement copysign for shaders (#158)
    * Implement copysign for shaders
    
    * Fix typo
    
    * PR feedback

--- rustc_codegen_spirv/src/builder/intrinsics.rs
+++ rustc_codegen_spirv/src/builder/intrinsics.rs
@@ -2,6 +2,7 @@ use super::Builder;
 use crate::abi::ConvSpirvType;
 use crate::builder_spirv::SpirvValueExt;
 use crate::codegen_cx::CodegenCx;
+use crate::spirv_type::SpirvType;
 use rspirv::spirv::{CLOp, GLOp};
 use rustc_codegen_ssa::mir::operand::OperandRef;
 use rustc_codegen_ssa::mir::place::PlaceRef;
@@ -240,7 +241,33 @@ impl<'a, 'tcx> IntrinsicCallMethods<'tcx> for Builder<'a, 'tcx> {
                 if self.kernel_mode {
                     self.cl_op(CLOp::copysign, [args[0].immediate(), args[1].immediate()])
                 } else {
-                    self.fatal("TODO: Shader copysign not supported yet https://github.com/EmbarkStudios/rust-gpu/issues/148")
+                    let val = args[0].immediate();
+                    let sign = args[1].immediate();
+                    let width = match self.lookup_type(val.ty) {
+                        SpirvType::Float(width) => width,
+                        other => bug!(
+                            "copysign must have float argument, not {}",
+                            other.debug(val.ty, self)
+                        ),
+                    };
+                    let int_ty = SpirvType::Integer(width, false).def(self);
+                    let (mask_sign, mask_value) = match width {
+                        32 => (
+                            self.constant_u32(1 << 31),
+                            self.constant_u32(u32::max_value() >> 1),
+                        ),
+                        64 => (
+                            self.constant_u64(1 << 63),
+                            self.constant_u64(u64::max_value() >> 1),
+                        ),
+                        _ => bug!("copysign must have width 32 or 64, not {}", width),
+                    };
+                    let val_bits = self.bitcast(val, int_ty);
+                    let sign_bits = self.bitcast(sign, int_ty);
+                    let val_masked = self.and(val_bits, mask_value);
+                    let sign_masked = self.and(sign_bits, mask_sign);
+                    let result_bits = self.or(val_masked, sign_masked);
+                    self.bitcast(result_bits, val.ty)
                 }
             }
             sym::floorf32 | sym::floorf64 => {
