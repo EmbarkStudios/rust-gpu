de4436d2e03ffd6dc432238c6491c72e811f6de0
Johan Andersson <repi@repi.se>
2020-10-27 22:23:00 +0100

    Fix compiling wgpu-example-shader for CPU

--- examples/wgpu-example-shader/src/lib.rs
+++ examples/wgpu-example-shader/src/lib.rs
@@ -1,12 +1,8 @@
-//! Ported to Rust from https://github.com/Tw1ddle/Sky-Shader/blob/master/src/shaders/glsl/sky.fragment
-
-#![no_std]
+#![cfg_attr(target_arch = "spirv", no_std)]
 #![feature(lang_items)]
 #![feature(register_attr)]
 #![register_attr(spirv)]
 
-#[cfg(not(test))]
-use core::panic::PanicInfo;
 use spirv_std::{Input, Output, Vec4};
 
 #[allow(unused_attributes)]
@@ -30,12 +26,12 @@ pub fn main_vs(
     ));
 }
 
-#[cfg(not(test))]
+#[cfg(all(not(test), target_arch = "spirv"))]
 #[panic_handler]
-fn panic(_: &PanicInfo) -> ! {
+fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-#[cfg(not(test))]
+#[cfg(all(not(test), target_arch = "spirv"))]
 #[lang = "eh_personality"]
 extern "C" fn rust_eh_personality() {}
