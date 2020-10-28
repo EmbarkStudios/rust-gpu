1920c6f39c726d861d55a1363135cf77e1ff5831
Gray Olson <gray@grayolson.com>
2020-10-27 19:30:55 -0700

    apply srgb OETF and invert clip space for cpu example runner

--- examples/example-runner-cpu/src/main.rs
+++ examples/example-runner-cpu/src/main.rs
@@ -3,13 +3,22 @@ use minifb::{Key, Window, WindowOptions};
 use rayon::prelude::*;
 use std::time::Instant;
 
+// apply the srgb OETF (i.e. do "linear to sRGB")
+fn srgb_oetf(x: f32) -> f32 {
+    if x <= 0.0031308 {
+        x * 12.92
+    } else {
+        1.055 * x.powf(1.0 / 2.4) - 0.055
+    }
+}
+
 fn color_u32_from_vec4(v: Vec4) -> u32 {
     let convert = |f: f32| -> u32 { (f.min(1.0).max(0.0) * 255.0).round() as u32 };
 
-    // this later will also do linear -> sRGB gamma correction for output,
-    // but currently our shader output is in sRGB so no conversion needed
-
-    convert(v.z()) | convert(v.y()) << 8 | convert(v.x()) << 16 | convert(v.w()) << 24
+    convert(srgb_oetf(v.z()))
+        | convert(srgb_oetf(v.y())) << 8
+        | convert(srgb_oetf(v.x())) << 16
+        | convert(v.w()) << 24
 }
 
 fn main() {
@@ -34,7 +43,7 @@ fn main() {
         .map(|i| {
             let screen_pos = vec2(
                 (i % WIDTH) as f32 / WIDTH as f32 * 2.0 - 1.0,
-                (i / WIDTH) as f32 / HEIGHT as f32 * 2.0 - 1.0,
+                -((i / WIDTH) as f32 / HEIGHT as f32 * 2.0 - 1.0),
             );
 
             // evaluate the fragment shader for the specific pixel
