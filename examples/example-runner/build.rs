439f7117451f27eaec0e10ed0b177e126e8144b9
Gray Olson <gray@grayolson.com>
2020-10-29 07:13:39 -0700

    restructure example directory structure according to #170

--- examples/example-runner/build.rs
+++
@@ -1,8 +0,0 @@
-use spirv_builder::SpirvBuilder;
-use std::error::Error;
-
-fn main() -> Result<(), Box<dyn Error>> {
-    // This will set the env var `example-shader.spv` to a spir-v file that can be include!()'d
-    SpirvBuilder::new("../sky-shader").build()?;
-    Ok(())
-}
--- examples/runners/wgpu/build.rs
+++ examples/runners/wgpu/build.rs
@@ -2,8 +2,8 @@ use spirv_builder::SpirvBuilder;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // This will set the env var `wgpu-example-shader.spv` to a spir-v file that can be include!()'d
-    SpirvBuilder::new("../sky-shader")
+    // This will set the env var `sky-shader.spv` to a spir-v file that can be include!()'d
+    SpirvBuilder::new("../../shaders/sky-shader")
         .spirv_version(1, 0)
         .build()?;
     Ok(())
