e72c77e391b38fe86db9e9977c8f3b0029cd6403
VZout <viktor.zoutman@embark-studios.com>
2020-10-30 11:51:48 +0100

    Added a test for a fully unreachable break scenario

--- spirv-builder/src/test/control_flow.rs
+++ spirv-builder/src/test/control_flow.rs
@@ -87,6 +87,23 @@ pub fn main(i: Input<i32>) {
 }
 
 #[test]
+fn cf_while_if_break_else_break() {
+    val(r#"
+#[allow(unused_attributes)]
+#[spirv(fragment)]
+pub fn main(i: Input<i32>) {
+    while i.load() < 10 {
+        if i.load() == 0 {
+            break;
+        } else {
+            break;
+        }
+    }
+}
+"#);
+}
+
+#[test]
 fn cf_if_while() {
     val(r#"
 #[allow(unused_attributes)]
