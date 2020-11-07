990bee5a556e9bc3ddcb97b2586f36ded4ac9cab
charles-r-earp <charles.r.earp@gmail.com>
2020-11-06 17:44:56 -0800

    Pulled in reorganization changes to crates + examples. In symbols.rs moved really_unsafe_ignore_bitcasts to its own Symbol match. In entry.rs, entry stubs now return the fn_id, so that entry_stub() can add the execution modes in one place. Passed all tests.

---
+++ examples/runners/wgpu-compute/src/main.rs
@@ -0,0 +1,74 @@
+
+async fn create_device_queue() -> (wgpu::Device, wgpu::Queue) {
+    let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: None,
+        })
+        .await
+        .expect("Failed to find an appropriate adapter");
+        
+    adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::default(),
+                shader_validation: true,
+            },
+            None,
+        )
+        .await
+        .expect("Failed to create device")
+}
+
+fn main() {
+    let (device, queue) = {
+        #[cfg(not(target_arch = "wasm32"))]
+        { futures::executor::block_on(create_device_queue()) }
+        #[cfg(target_arch = "wasm32")]
+        { wasm_bindgen_futures::spawn_local(create_device_queue()) }
+    };
+    
+    // Load the shaders from disk
+    let module = device.create_shader_module(wgpu::include_spirv!(env!("compute_shader.spv")));
+    
+    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        label: None,
+        entries: &[],
+    });
+        
+    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: None,
+        bind_group_layouts: &[&bind_group_layout],
+        push_constant_ranges: &[],
+    });    
+        
+    let compute_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+        label: None,
+        layout: Some(&pipeline_layout),
+        compute_stage: wgpu::ProgrammableStageDescriptor {
+            module: &module,
+            entry_point: "main_cs",
+        },
+    });
+    
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: None,
+        layout: &bind_group_layout,
+        entries: &[],
+    });
+    
+    let mut encoder = device.create_command_encoder(
+        &wgpu::CommandEncoderDescriptor { label: None }
+    );
+    
+    {
+        let mut cpass = encoder.begin_compute_pass();
+        cpass.set_bind_group(0, &bind_group, &[]);
+        cpass.set_pipeline(&compute_pipeline);
+        cpass.dispatch(1, 1, 1);
+    }
+    
+    queue.submit(Some(encoder.finish()));
+}
