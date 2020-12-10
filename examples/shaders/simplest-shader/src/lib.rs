57b49d932af5b0ee37714dbbc0a6bfafd8a7ebce
DGriffin91 <33357138+DGriffin91@users.noreply.github.com>
2020-12-09 23:25:06 -0800

    Update shared to use shorter glam vec functions for consistency (#333)
    Co-authored-by: DGriffin91 <git@dgdigital.net>

--- examples/shaders/simplest-shader/src/lib.rs
+++ examples/shaders/simplest-shader/src/lib.rs
@@ -2,13 +2,13 @@
 #![feature(register_attr)]
 #![register_attr(spirv)]
 
-use spirv_std::glam::Vec4;
+use spirv_std::glam::{vec4, Vec4};
 use spirv_std::storage_class::{Input, Output};
 
 #[allow(unused_attributes)]
 #[spirv(fragment)]
 pub fn main_fs(mut output: Output<Vec4>) {
-    output.store(Vec4::new(1.0, 0.0, 0.0, 1.0))
+    output.store(vec4(1.0, 0.0, 0.0, 1.0))
 }
 
 #[allow(unused_attributes)]
@@ -18,7 +18,7 @@ pub fn main_vs(
     #[spirv(position)] mut out_pos: Output<Vec4>,
 ) {
     let vert_id = vert_id.load();
-    out_pos.store(Vec4::new(
+    out_pos.store(vec4(
         (vert_id - 1) as f32,
         ((vert_id & 1) * 2 - 1) as f32,
         0.0,
