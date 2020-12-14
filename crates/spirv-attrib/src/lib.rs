6d4a9ce1f7537342bc8d63f385f80a9687681acd
VZout <viktor.zoutman@embark-studios.com>
2020-12-14 14:43:17 +0100

    cleanup

--- crates/spirv-attrib/src/lib.rs
+++ crates/spirv-attrib/src/lib.rs
@@ -5,34 +5,31 @@ use proc_macro::{Delimiter, Group, TokenStream, TokenTree};
 pub fn spirv(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut tokens = Vec::new();
     for tt in item {
-        match tt {
-            TokenTree::Group(group) => match group.delimiter() {
-                Delimiter::Parenthesis => {
-                    let mut sub_tokens = Vec::new();
-                    for tt in group.stream() {
-                        match tt {
-                            TokenTree::Group(group) => match group.delimiter() {
-                                Delimiter::Bracket => {
-                                    if group.stream().to_string().starts_with("spirv") {
-                                        sub_tokens.pop();
-                                    } else {
-                                        sub_tokens.push(TokenTree::from(group));
-                                    }
-                                }
-                                _ => sub_tokens.push(TokenTree::from(group)),
-                            },
-                            _ => sub_tokens.push(tt),
+        if let TokenTree::Group(group) = tt {
+            if let Delimiter::Parenthesis = group.delimiter() {
+                let mut sub_tokens = Vec::new();
+                for tt in group.stream() {
+                    if let TokenTree::Group(group) = tt {
+                        if let Delimiter::Bracket = group.delimiter() {
+                            if group.stream().to_string().starts_with("spirv") {
+                                sub_tokens.pop();
+                            } else {
+                                sub_tokens.push(TokenTree::from(group));
+                            }
                         }
+                    } else {
+                        sub_tokens.push(tt);
                     }
-
-                    tokens.push(TokenTree::from(Group::new(
-                        Delimiter::Parenthesis,
-                        TokenStream::from_iter(sub_tokens),
-                    )));
                 }
-                _ => tokens.push(TokenTree::from(group)),
-            },
-            _ => tokens.push(tt),
+                tokens.push(TokenTree::from(Group::new(
+                    Delimiter::Parenthesis,
+                    TokenStream::from_iter(sub_tokens),
+                )));
+            } else {
+                tokens.push(TokenTree::from(group));
+            }
+        } else {
+            tokens.push(tt);
         }
     }
 
