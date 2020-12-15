ad98a58930a9842306ae1dbb945865b0f450af93
VZout <viktor.zoutman@embark-studios.com>
2020-12-15 11:17:47 +0100

    Feedback

--- crates/spirv-attrib/src/lib.rs
+++ crates/spirv-attrib/src/lib.rs
@@ -1,36 +1,31 @@
 use core::iter::FromIterator;
+use core::iter::IntoIterator;
 use proc_macro::{Delimiter, Group, TokenStream, TokenTree};
 
 #[proc_macro_attribute]
 pub fn spirv(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut tokens = Vec::new();
     for tt in item {
-        if let TokenTree::Group(group) = tt {
+        if let TokenTree::Group(group) = tt.clone() {
             if let Delimiter::Parenthesis = group.delimiter() {
                 let mut sub_tokens = Vec::new();
                 for tt in group.stream() {
-                    if let TokenTree::Group(group) = tt {
-                        if let Delimiter::Bracket = group.delimiter() {
-                            if group.stream().to_string().starts_with("spirv") {
-                                sub_tokens.pop();
-                            } else {
-                                sub_tokens.push(TokenTree::from(group));
-                            }
-                        }
-                    } else {
-                        sub_tokens.push(tt);
+                    if matches!(tt.clone(), TokenTree::Group(group) if group.delimiter() == Delimiter::Bracket && matches!(group.stream().into_iter().next(), Some(TokenTree::Ident(ident)) if ident.to_string() == "spirv"))
+                        && matches!(sub_tokens.last(), Some(TokenTree::Punct(p)) if p.as_char() == '#')
+                    {
+                        sub_tokens.pop();
+                        continue;
                     }
+                    sub_tokens.push(tt);
                 }
                 tokens.push(TokenTree::from(Group::new(
                     Delimiter::Parenthesis,
                     TokenStream::from_iter(sub_tokens),
                 )));
-            } else {
-                tokens.push(TokenTree::from(group));
+                continue;
             }
-        } else {
-            tokens.push(tt);
         }
+        tokens.push(tt);
     }
 
     TokenStream::from_iter(tokens)
