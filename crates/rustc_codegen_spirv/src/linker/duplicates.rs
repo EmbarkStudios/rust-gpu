0f7b59f64cfd4f236afa68afbb0af7b2ca20a89f
khyperia <github@khyperia.com>
2021-01-28 13:09:16 +0100

    Remove different OpNames that target the same ID

--- crates/rustc_codegen_spirv/src/linker/duplicates.rs
+++ crates/rustc_codegen_spirv/src/linker/duplicates.rs
@@ -269,8 +269,8 @@ pub fn remove_duplicate_types(module: &mut Module) {
         .annotations
         .retain(|inst| anno_set.insert(inst.assemble()));
     // Same thing with OpName
-    let mut debug_set = HashSet::new();
-    module
-        .debugs
-        .retain(|inst| debug_set.insert(inst.assemble()));
+    let mut name_ids = HashSet::new();
+    module.debugs.retain(|inst| {
+        inst.class.opcode != Op::Name || name_ids.insert(inst.operands[0].unwrap_id_ref())
+    });
 }
