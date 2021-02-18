55668118c432f831a864492d734dd63f198e1153
Henno <henno.trost@gmail.com>
2021-02-18 18:55:29 -0500

    Duplicate SpirvBuilder compilation logic
    Duplicate the compilation logic from spirv-builder to circumvent cargo issues.

--- examples/runners/wgpu/src/lib.rs
+++ examples/runners/wgpu/src/lib.rs
@@ -14,20 +14,59 @@ pub enum RustGPUShader {
 
 #[cfg(not(any(target_os = "android", target_arch = "wasm32")))]
 fn compile_shader(shader: RustGPUShader) -> Vec<u8> {
-    let compile_spv_module = |path_to_src: &str| {
-        let spv_path = spirv_builder::SpirvBuilder::new(path_to_src)
-            .spirv_version(1, 0)
-            .print_metadata(false)
-            .build()
-            .expect("failed to compile spirv");
-        std::fs::read(spv_path).expect("failed to read .spv file")
+    let path_to_src = match shader {
+        RustGPUShader::Simplest => "examples/shaders/simplest-shader",
+        RustGPUShader::Sky => "examples/shaders/sky-shader",
+        RustGPUShader::Compute => "examples/shaders/compute-shader",
+        RustGPUShader::Mouse => "examples/shaders/mouse-shader",
     };
-    match shader {
-        RustGPUShader::Simplest => compile_spv_module("examples/shaders/simplest-shader"),
-        RustGPUShader::Sky => compile_spv_module("examples/shaders/sky-shader"),
-        RustGPUShader::Compute => compile_spv_module("examples/shaders/compute-shader"),
-        RustGPUShader::Mouse => compile_spv_module("examples/shaders/mouse-shader"),
-    }
+
+    // rustc_codegen_spirv gets built by the spirv-builder build dependency
+    // the resulting dll needs to be retrieved directly or IC won't work
+    let rustc_codegen_spirv = {
+        let dll = format!(
+            "{}rustc_codegen_spirv{}",
+            std::env::consts::DLL_PREFIX,
+            std::env::consts::DLL_SUFFIX,
+        );
+        let release = std::path::PathBuf::from("target/release").join(&dll);
+        let debug = std::path::PathBuf::from("target/debug").join(&dll);
+        if release.exists() {
+            release.canonicalize().unwrap()
+        } else if debug.exists() {
+            debug.canonicalize().unwrap()
+        } else {
+            // cargo will search a series of directories (including the rustup tools directory),
+            // but IC won't work unless it's located in the tools directory.
+            std::path::PathBuf::from(dll)
+        }
+    };
+
+    std::process::Command::new("cargo")
+        .stderr(std::process::Stdio::inherit())
+        .env("CARGO_INCREMENTAL", "1")
+        .args(&[
+            "build",
+            "-Z",
+            "build-std=core",
+            "--target",
+            "spirv-unknown-unknown",
+            "--release",
+            "--manifest-path",
+            &format!("{}/Cargo.toml", path_to_src),
+        ])
+        .env("RUSTFLAGS", format!(
+            "-Z codegen-backend={} -Z symbol-mangling-version=v0 -C target-feature=+spirv1.0,+vulkan",
+            rustc_codegen_spirv.display(),
+        ))
+        .output()
+        .expect("failed to execute cargo build");
+
+    std::fs::read(&format!(
+        "target/spirv-unknown-unknown/release/{}.spv",
+        path_to_src.split("/").last().unwrap().replace("-", "_")
+    ))
+    .expect("failed to read .spv file")
 }
 
 #[cfg(any(target_os = "android", target_arch = "wasm32"))]
