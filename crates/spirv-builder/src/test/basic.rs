6a39f8ec9dfb623034d4907e7d0ad02572f19ff0
Jasper Bekkers <bekkers@gmail.com>
2021-02-18 13:35:42 +0100

    Add test for a complex OpImageSample instruction that needs some BitEnums

--- crates/spirv-builder/src/test/basic.rs
+++ crates/spirv-builder/src/test/basic.rs
@@ -493,3 +493,72 @@ pub fn main(input: Input<glam::Mat3>, mut output: Output<glam::Vec3>) {
 }
 "#);
 }
+
+#[test]
+fn complex_image_sample_inst() {
+    dis_fn(
+        r#"
+    fn sample_proj_lod(coord: glam::Vec4, ddx: glam::Vec2, ddy: glam::Vec2, offset_x: i32, offset_y: i32) -> glam::Vec4 {
+        unsafe {
+            let mut result = glam::Vec4::default();
+            let index = 0u32;
+            asm!(
+                    "OpExtension \"SPV_EXT_descriptor_indexing\"",
+                    "OpCapability RuntimeDescriptorArray",
+                    "OpDecorate %image_2d_var DescriptorSet 0",
+                    "OpDecorate %image_2d_var Binding 0",
+                    "%uint                  = OpTypeInt 32 0",
+                    "%int                   = OpTypeInt 32 1",
+                    "%float                 = OpTypeFloat 32",
+                    "%v2int                 = OpTypeVector %int 2",
+                    "%uint_0                = OpConstant %uint 0",
+                    "%int_0                 = OpConstant %int 0",
+                    "%image_2d              = OpTypeImage %float Dim2D 0 0 0 1 Unknown",
+                    "%sampled_image_2d      = OpTypeSampledImage %image_2d",
+                    "%image_array           = OpTypeRuntimeArray %sampled_image_2d",
+                    "%ptr_image_array       = OpTypePointer UniformConstant %image_array",
+                    "%image_2d_var          = OpVariable %ptr_image_array UniformConstant",
+                    "%ptr_sampled_image_2d  = OpTypePointer UniformConstant %sampled_image_2d",
+                    "", // ^^ type preamble
+                    "%offset                = OpLoad _ {1}",
+                    "%24                    = OpAccessChain %ptr_sampled_image_2d %image_2d_var %offset",
+                    "%25                    = OpLoad %sampled_image_2d %24",
+                    "%coord                 = OpLoad _ {0}",
+                    "%ddx                   = OpLoad _ {3}",
+                    "%ddy                   = OpLoad _ {4}",
+                    "%offset_x              = OpLoad _ {5}",
+                    "%offset_y              = OpLoad _ {6}",
+                    "%const_offset          = OpConstantComposite %v2int %int_0 %int_0",
+                    "%result                = OpImageSampleProjExplicitLod _ %25 %coord Grad|ConstOffset %ddx %ddy %const_offset",
+                    "OpStore {2} %result",
+                    in(reg) &coord,
+                    in(reg) &index,
+                    in(reg) &mut result,
+                    in(reg) &ddx,
+                    in(reg) &ddy,
+                    in(reg) &offset_x,
+                    in(reg) &offset_y,
+                );
+            result
+        }
+    }
+    #[allow(unused_attributes)]
+    #[spirv(fragment)]
+    pub fn main() {
+        sample_proj_lod(glam::Vec4::zero(), glam::Vec2::zero(), glam::Vec2::zero(), 0, 0);
+    }"#,
+        "sample_proj_lod",
+        "%1 = OpFunction %2 None %3
+%4 = OpFunctionParameter %2
+%5 = OpFunctionParameter %6
+%7 = OpFunctionParameter %6
+%8 = OpFunctionParameter %9
+%10 = OpFunctionParameter %9
+%11 = OpLabel
+%12 = OpAccessChain %13 %14 %15
+%16 = OpLoad %17 %12
+%18 = OpImageSampleProjExplicitLod %2 %16 %4 Grad|ConstOffset %5 %7 %19
+OpReturnValue %18
+OpFunctionEnd",
+    );
+}
