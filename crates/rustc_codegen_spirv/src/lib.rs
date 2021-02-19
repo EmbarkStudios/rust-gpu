d1765b53f88ddbe16aac2aa55faf680edc03d0a4
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2021-02-19 13:14:09 +0200

    Work around linker error by isolating spirv_tools usage to rustc_codegen_spirv.

--- crates/rustc_codegen_spirv/src/lib.rs
+++ crates/rustc_codegen_spirv/src/lib.rs
@@ -121,7 +121,6 @@ use rustc_session::Session;
 use rustc_span::symbol::{sym, Symbol};
 use rustc_target::spec::abi::Abi;
 use rustc_target::spec::{LinkerFlavor, PanicStrategy, Target, TargetOptions, TargetTriple};
-pub use spirv_tools;
 use std::any::Any;
 use std::env;
 use std::fs::{create_dir_all, File};
@@ -608,4 +607,18 @@ pub fn __rustc_codegen_backend() -> Box<dyn CodegenBackend> {
     }));
 
     Box::new(SpirvCodegenBackend)
+}
+
+// HACK(eddyb) this allows `spirv-builder` to use `spirv-tools::val` without
+// risking linker errors (especially when compiled with optimizations) - this
+// also means the function can't be generic or `#[inline]`.
+pub use spirv_tools::TargetEnv as SpirvToolsTargetEnv;
+#[inline(never)]
+pub fn spirv_tools_validate(
+    target_env: Option<spirv_tools::TargetEnv>,
+    bytes: &[u8],
+    options: Option<spirv_tools::val::ValidatorOptions>,
+) -> Result<(), spirv_tools::error::Error> {
+    use spirv_tools::val::Validator as _;
+    spirv_tools::val::create(target_env).validate(spirv_tools::binary::to_binary(bytes)?, options)
 }
