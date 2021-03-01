2f4c67e8e0570fdfe016e230677d1a4a136b3fb2
Jasper Bekkers <Jasper-Bekkers@users.noreply.github.com>
2021-03-01 16:20:07 +0100

    Safeness bug in StorageImage2d::write (#453)
    * Safeness bug in StorageImage2d::write
    
    It's pretty easy to see why this is unsafe, if multiple threads write to the same coordinate race-conditions happen.
    
    Ultimately this should be addressed through something like #216 and some higher level abstractions on top of our buffer types, but since we don't have those for now, marking this as unsafe seems to be the only thing we can do for now.
    
    * Remove unsafe {} block for clippy

--- crates/spirv-std/src/textures.rs
+++ crates/spirv-std/src/textures.rs
@@ -89,22 +89,20 @@ impl StorageImage2d {
 
     /// Write a texel to an image without a sampler.
     #[spirv_std_macros::gpu_only]
-    pub fn write<I, V, V2, const N: usize>(&self, coordinate: V, texels: V2)
+    pub unsafe fn write<I, V, V2, const N: usize>(&self, coordinate: V, texels: V2)
     where
         I: Integer,
         V: Vector<I, 2>,
         V2: Vector<f32, N>,
     {
-        unsafe {
-            asm! {
-                "%image = OpLoad _ {this}",
-                "%coordinate = OpLoad _ {coordinate}",
-                "%texels = OpLoad _ {texels}",
-                "OpImageWrite %image %coordinate %texels",
-                this = in(reg) self,
-                coordinate = in(reg) &coordinate,
-                texels = in(reg) &texels,
-            }
+        asm! {
+            "%image = OpLoad _ {this}",
+            "%coordinate = OpLoad _ {coordinate}",
+            "%texels = OpLoad _ {texels}",
+            "OpImageWrite %image %coordinate %texels",
+            this = in(reg) self,
+            coordinate = in(reg) &coordinate,
+            texels = in(reg) &texels,
         }
     }
 }
