6e1d3e46d5ef12fec5319b2a8b2e18aa4534683a
khyperia <github@khyperia.com>
2021-03-11 13:28:00 +0000

    Deploying to gh-pages from  @ cb8477df150199612ded1ac7f233732fd54f2081 🚀

--- api/spirv_std_macros/sidebar-items.js
+++ api/spirv_std_macros/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"attr":[["gpu_only",""],["spirv",""]]});
+initSidebarItems({"attr":[["gpu_only","Marks a function as runnable only on the GPU, and will panic on CPU platforms."],["spirv",""]]});
