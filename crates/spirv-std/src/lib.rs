a173208d803aeac9295464df703c53d987db2664
Johan Andersson <johan@embark-studios.com>
2021-03-12 13:35:37 +0100

    Update to Embark lints v0.3 (#482)
    Co-Authored-By: Johan Andersson <repi@repi.se>
    
    Co-authored-by: Erin Power <erin.power@embark-studios.com>

--- crates/spirv-std/src/lib.rs
+++ crates/spirv-std/src/lib.rs
@@ -4,7 +4,7 @@
     feature(asm, register_attr, repr_simd, core_intrinsics, lang_items),
     register_attr(spirv)
 )]
-// BEGIN - Embark standard lints v0.2.
+// BEGIN - Embark standard lints v0.3
 // do not change or add/remove here, but one can add exceptions after this section
 // for more info see: <https://github.com/EmbarkStudios/rust-ecosystem/issues/59>
 #![deny(unsafe_code)]
@@ -23,13 +23,16 @@
     clippy::if_let_mutex,
     clippy::imprecise_flops,
     clippy::inefficient_to_string,
+    clippy::large_types_passed_by_value,
     clippy::let_unit_value,
     clippy::linkedlist,
     clippy::lossy_float_literal,
     clippy::macro_use_imports,
+    clippy::map_err_ignore,
     clippy::map_flatten,
     clippy::map_unwrap_or,
     clippy::match_on_vec_items,
+    clippy::match_same_arms,
     clippy::match_wildcard_for_single_variants,
     clippy::mem_forget,
     clippy::mismatched_target_os,
@@ -39,9 +42,12 @@
     clippy::pub_enum_variant_names,
     clippy::ref_option_ref,
     clippy::rest_pat_in_fully_bound_structs,
+    clippy::string_add_assign,
+    clippy::string_add,
     clippy::string_to_string,
     clippy::suboptimal_flops,
     clippy::todo,
+    clippy::unimplemented,
     clippy::unnested_or_patterns,
     clippy::unused_self,
     clippy::verbose_file_reads,
@@ -49,9 +55,15 @@
     nonstandard_style,
     rust_2018_idioms
 )]
-// END - Embark standard lints v0.2
+// END - Embark standard lints v0.3
 // crate-specific exceptions:
-#![allow(unsafe_code)] // still quite a bit needed
+#![allow(
+    // Needed for `asm!`.
+    unsafe_code,
+    // We deblierately provide an unimplemented version of our API on CPU
+    // platforms so that code completion still works.
+    clippy::unimplemented,
+)]
 
 #[cfg(not(target_arch = "spirv"))]
 #[macro_use]
