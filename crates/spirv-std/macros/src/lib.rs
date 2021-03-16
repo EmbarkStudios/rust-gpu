4c0ca81d0e8062723c1e0eb168a9c3e0b0cd9eff
Erin Power <erin.power@embark-studios.com>
2021-03-16 11:26:27 +0100

    Release 0.3.1
    compute-shader@0.3.1
    rustc_codegen_spirv@0.3.1
    spirv-builder@0.3.1
    spirv-std@0.3.1
    spirv-std-macros@0.3.1
    
    Generated by cargo-workspaces

---
+++ crates/spirv-std/macros/src/lib.rs
@@ -0,0 +1,107 @@
+// BEGIN - Embark standard lints v0.2.
+// do not change or add/remove here, but one can add exceptions after this section
+// for more info see: <https://github.com/EmbarkStudios/rust-ecosystem/issues/59>
+#![deny(unsafe_code)]
+#![warn(
+    clippy::all,
+    clippy::await_holding_lock,
+    clippy::dbg_macro,
+    clippy::debug_assert_with_mut_call,
+    clippy::doc_markdown,
+    clippy::empty_enum,
+    clippy::enum_glob_use,
+    clippy::exit,
+    clippy::explicit_into_iter_loop,
+    clippy::filter_map_next,
+    clippy::fn_params_excessive_bools,
+    clippy::if_let_mutex,
+    clippy::imprecise_flops,
+    clippy::inefficient_to_string,
+    clippy::let_unit_value,
+    clippy::linkedlist,
+    clippy::lossy_float_literal,
+    clippy::macro_use_imports,
+    clippy::map_flatten,
+    clippy::map_unwrap_or,
+    clippy::match_on_vec_items,
+    clippy::match_wildcard_for_single_variants,
+    clippy::mem_forget,
+    clippy::mismatched_target_os,
+    clippy::needless_borrow,
+    clippy::needless_continue,
+    clippy::option_option,
+    clippy::pub_enum_variant_names,
+    clippy::ref_option_ref,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::string_to_string,
+    clippy::suboptimal_flops,
+    clippy::todo,
+    clippy::unnested_or_patterns,
+    clippy::unused_self,
+    clippy::verbose_file_reads,
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms
+)]
+// END - Embark standard lints v0.2
+// crate-specific exceptions:
+#![allow()]
+
+use proc_macro::{Delimiter, Group, TokenStream, TokenTree};
+
+#[proc_macro_attribute]
+pub fn spirv(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut tokens = Vec::new();
+    for tt in item {
+        match tt {
+            TokenTree::Group(group) if group.delimiter() == Delimiter::Parenthesis => {
+                let mut sub_tokens = Vec::new();
+                for tt in group.stream() {
+                    match tt {
+                        TokenTree::Group(group)
+                            if group.delimiter() == Delimiter::Bracket
+                                && matches!(group.stream().into_iter().next(), Some(TokenTree::Ident(ident)) if ident.to_string() == "spirv")
+                                && matches!(sub_tokens.last(), Some(TokenTree::Punct(p)) if p.as_char() == '#') =>
+                        {
+                            sub_tokens.pop();
+                        }
+                        _ => sub_tokens.push(tt),
+                    }
+                }
+                tokens.push(TokenTree::from(Group::new(
+                    Delimiter::Parenthesis,
+                    sub_tokens.into_iter().collect(),
+                )));
+            }
+            _ => tokens.push(tt),
+        }
+    }
+    tokens.into_iter().collect()
+}
+
+/// Marks a function as runnable only on the GPU, and will panic on
+/// CPU platforms.
+#[proc_macro_attribute]
+pub fn gpu_only(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let syn::ItemFn {
+        attrs,
+        vis,
+        sig,
+        block,
+    } = syn::parse_macro_input!(item as syn::ItemFn);
+
+    let fn_name = sig.ident.clone();
+
+    let output = quote::quote! {
+        // Don't warn on unused arguments on the CPU side.
+        #[cfg_attr(not(target_arch = "spirv"), allow(unused_variables))]
+        #(#attrs)* #vis #sig {
+            #[cfg(target_arch="spirv")] { #block }
+            #[cfg(not(target_arch="spirv"))] {
+                unimplemented!(concat!("`", stringify!(#fn_name), "` is only available on SPIR-V platforms."))
+            }
+        }
+    };
+
+    output.into()
+}
