fa26785a6bb5bc6e5d42e0c034b6b97005817379
khyperia <github@khyperia.com>
2021-03-30 08:36:11 +0200

    Run DCE twice

--- crates/spirv-builder/src/test/basic.rs
+++ crates/spirv-builder/src/test/basic.rs
@@ -189,15 +189,14 @@ OpDecorate %5 Binding 0
 %6 = OpTypeVoid
 %7 = OpTypeFunction %6
 %8 = OpTypeInt 32 0
-%9 = OpTypePointer Function %8
-%10 = OpConstant %8 1
-%11 = OpTypeFloat 32
-%12 = OpTypeImage %11 2D 0 0 0 1 Unknown
-%13 = OpTypeSampledImage %12
-%4 = OpTypeRuntimeArray %13
-%14 = OpTypePointer UniformConstant %4
-%5 = OpVariable %14 UniformConstant
-%15 = OpTypePointer UniformConstant %13"#,
+%9 = OpConstant %8 1
+%10 = OpTypeFloat 32
+%11 = OpTypeImage %10 2D 0 0 0 1 Unknown
+%12 = OpTypeSampledImage %11
+%4 = OpTypeRuntimeArray %12
+%13 = OpTypePointer UniformConstant %4
+%5 = OpVariable %13 UniformConstant
+%14 = OpTypePointer UniformConstant %12"#,
     );
 }
 
