ac73845d462b18cc521594bbe5182ee0a8f581a5
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2021-03-30 11:28:42 +0000

    Deploying to gh-pages from  @ f09a5f6e65dd54ef1b825d1bd3987da089c89cd9 🚀

---
+++ api/convert_case/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Case","Defines the type of casing a string can be."]],"struct":[["FromCasing","Holds information about parsing before converting into a case."]],"trait":[["Casing","Describes items that can be converted into a case."]]});
---
+++ api/implementors/convert_case/trait.Casing.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["convert_case"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
