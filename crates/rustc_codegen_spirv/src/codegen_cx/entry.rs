2f898937713a8260d902a8d7554761049def1950
Jasper Bekkers <Jasper-Bekkers@users.noreply.github.com>
2021-04-12 14:37:48 +0200

    Remove unused bindless_mode setting

--- crates/rustc_codegen_spirv/src/codegen_cx/entry.rs
+++ crates/rustc_codegen_spirv/src/codegen_cx/entry.rs
@@ -104,13 +104,7 @@ impl<'tcx> CodegenCx<'tcx> {
                 emit.execution_mode(fn_id, *execution_mode, execution_mode_extra);
             });
     }
-
-    pub fn bindless_mode(&self) -> bool {
-        self.tcx
-            .sess
-            .contains_name(self.tcx.hir().krate_attrs(), self.sym.spirv_bindless)
-    }
-
+    
     pub fn lazy_add_bindless_descriptor_sets(&self) {
         self.bindless_descriptor_sets
             .replace(Some(BindlessDescriptorSets {
