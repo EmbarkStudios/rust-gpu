f9b4737b6d075899ec2a27c798ede3a7b34fd854
XAMPPRocky <4464295+XAMPPRocky@users.noreply.github.com>
2021-04-16 11:51:02 +0200

    Update spirv_type_constraints.rs

--- crates/rustc_codegen_spirv/src/spirv_type_constraints.rs
+++ crates/rustc_codegen_spirv/src/spirv_type_constraints.rs
@@ -757,7 +757,7 @@ pub fn instruction_signatures(op: Op) -> Option<&'static [InstSig<'static>]> {
         | Op::ExecuteCallableNV => {
             // NOTE(eddyb) Some KHR variants are aliased to the the NV instructions.
             // reserved!(SPV_KHR_ray_tracing)
-        },
+        }
         // SPV_NV_cooperative_matrix
         Op::TypeCooperativeMatrixNV
         | Op::CooperativeMatrixLoadNV
