cdaefaa8b1f678fbe43b6edf4115694119f9bd8f
khyperia <github@khyperia.com>
2021-05-20 12:52:13 +0000

    Deploying to gh-pages from  @ ca988f95decff252dbbd7912d664b5d7b9d13033 🚀

--- api/implementors/std/sys/unix/ext/io/trait.AsRawFd.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["tempfile"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/unix/ext/io/trait.AsRawFd.html\" title=\"trait std::sys::unix::ext::io::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"tempfile/struct.NamedTempFile.html\" title=\"struct tempfile::NamedTempFile\">NamedTempFile</a>","synthetic":false,"types":["tempfile::file::NamedTempFile"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
