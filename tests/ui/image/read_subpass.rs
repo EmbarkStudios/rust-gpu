837aadb5c14cbdf38e0d6ded941f3cb912a0e5b9
Alex S <alex0player@gmail.com>
2021-06-02 03:42:51 +0300

    Rustfmt

--- tests/ui/image/read_subpass.rs
+++ tests/ui/image/read_subpass.rs
@@ -4,8 +4,7 @@ use spirv_std::{arch, Image};
 
 #[spirv(fragment)]
 pub fn main(
-    #[spirv(descriptor_set = 0, binding = 0, input_attachment_index = 0)]
-    image: &Image!(subpass, type=f32, sampled=false),
+    #[spirv(descriptor_set = 0, binding = 0, input_attachment_index = 0)] image: &Image!(subpass, type=f32, sampled=false),
     output: &mut glam::Vec4,
 ) {
     let coords = image.read_subpass(glam::IVec2::new(0, 0));
