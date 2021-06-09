ce7a98c6d8a8c106d6d462bf3a5a0fcea3462ea0
khyperia <github@khyperia.com>
2021-06-09 11:51:11 +0200

    Formatting

--- crates/spirv-std/macros/src/lib.rs
+++ crates/spirv-std/macros/src/lib.rs
@@ -89,18 +89,18 @@ use quote::ToTokens;
 /// - `dimensionality` — Dimensionality of an image. Accepted values: `1D`,
 ///   `2D`, `3D`, `rect`, `cube`, `subpass`.
 /// - `type` — The sampled type of an image, mutually exclusive with `format`,
-///   when set the image format is unknown.  Accepted values: `f32`, `f64`,
+///   when set the image format is unknown. Accepted values: `f32`, `f64`,
 ///   `u8`, `u16`, `u32`, `u64`, `i8`, `i16`, `i32`, `i64`.
-/// - `format` — The image format of the image, mutually exclusive with `type`,
+/// - `format` — The image format of the image, mutually exclusive with `type`.
 ///   Accepted values: Camel case versions of [`ImageFormat`].
 /// - `sampled` — Whether it is known that the image will be used with a sampler
-///   at compile time, Accepted values: `true` or `false`. Default: `unknown`.
+///   at compile time. Accepted values: `true` or `false`. Default: `unknown`.
 /// - `multisampled` — Whether the image contains multisampled content. Accepted
 ///    values: `true` or `false`. Default: `false`.
 /// - `arrayed` — Whether the image contains arrayed content. Accepted
 ///    values: `true` or `false`. Default: `false`.
-/// - `depth` — Whether it is known that the image is a depth image,
-///   Accepted values: `true` or `false`. Default: `unknown`.
+/// - `depth` — Whether it is known that the image is a depth image.
+///    Accepted values: `true` or `false`. Default: `unknown`.
 ///
 /// [`ImageFormat`]: spirv_types::image_params::ImageFormat
 #[proc_macro]
