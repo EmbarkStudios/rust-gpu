03ca4c4c7ff5739cbe2316788786ff0edfb19fd5
khyperia <github@khyperia.com>
2021-06-09 07:37:35 +0000

    Deploying to gh-pages from  @ e3720294de2869ee84ec438a82046c2b80446a91 🚀

---
+++ api/glam/bool/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["BVec2","A 2-dimensional boolean vector."],["BVec3","A 3-dimensional boolean vector."],["BVec3A","A 3-dimensional SIMD vector mask."],["BVec4","A 4-dimensional boolean vector."],["BVec4A","A 4-dimensional SIMD vector mask."]]});
---
+++ api/glam/swizzles/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"trait":[["Vec2Swizzles","Swizzle methods for 2-dimensional vector types. "],["Vec3Swizzles","Swizzle methods for 3-dimensional vector types. "],["Vec4Swizzles","Swizzle methods for 3-dimensional vector types. "]]});
