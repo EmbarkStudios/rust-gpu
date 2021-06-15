04dfa80266b3efd0f44acfa941e6a20a16bf00ae
Ashley Hauck <github@khyperia.com>
2021-06-15 09:43:13 +0200

    Nuke arithmetic.rs (#666)

--- tests/ui/arch/vector_times_scalar.rs
+++
@@ -1,8 +0,0 @@
-// build-pass
-
-#[spirv(fragment)]
-pub fn main() {
-    let vector = glam::Vec2::new(10.0, 10.0);
-    let scalar = 2.0;
-    assert!(spirv_std::arch::vector_times_scalar(vector, scalar) == glam::Vec2::new(20.0, 20.0));
-}
