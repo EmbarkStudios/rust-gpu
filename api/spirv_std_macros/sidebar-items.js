0069028c001387a9664aa605ca37d247098fdf18
khyperia <github@khyperia.com>
2021-06-21 12:38:39 +0000

    Deploying to gh-pages from  @ 22e4bf4022c0457f67b29c3b32c196dcbeb0a697 🚀

--- api/spirv_std_macros/sidebar-items.js
+++ api/spirv_std_macros/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"attr":[["gpu_only","Marks a function as runnable only on the GPU, and will panic on CPU platforms."],["spirv",""]],"macro":[["Image","A macro for creating SPIR-V `OpTypeImage` types."]]});
+initSidebarItems({"attr":[["gpu_only","Marks a function as runnable only on the GPU, and will panic on CPU platforms."],["spirv",""]],"macro":[["Image","A macro for creating SPIR-V `OpTypeImage` types. Always produces a `spirv_std::image::Image<...>` type."]]});
