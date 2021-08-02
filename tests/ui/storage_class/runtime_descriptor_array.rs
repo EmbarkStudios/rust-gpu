054d0207ef5900345774907f8bcb73cebec83361
khyperia <github@khyperia.com>
2021-08-02 15:25:09 +0200

    Fix tests from glam breaking change

--- tests/ui/storage_class/runtime_descriptor_array.rs
+++ tests/ui/storage_class/runtime_descriptor_array.rs
@@ -1,4 +1,6 @@
 // build-pass
+// compile-flags: -C target-feature=+RuntimeDescriptorArray,+ext:SPV_EXT_descriptor_indexing
+
 use spirv_std::{Image, RuntimeArray, Sampler};
 
 #[spirv(fragment)]
@@ -8,16 +10,12 @@ pub fn main(
     #[spirv(descriptor_set = 0, binding = 2)] sized_slice: &[Image!(2D, type=f32, sampled); 5],
     output: &mut glam::Vec4,
 ) {
-    unsafe {
-        asm!(
-            "OpCapability RuntimeDescriptorArray",
-            "OpExtension \"SPV_EXT_descriptor_indexing\""
-        )
-    }
     let img = unsafe { slice.index(5) };
     let v2 = glam::Vec2::new(0.0, 1.0);
-    *output = img.sample(*sampler, v2);
+    let r1: glam::Vec4 = img.sample(*sampler, v2);
 
     let img_2 = &sized_slice[2];
-    *output += img_2.sample(*sampler, v2);
+    let r2: glam::Vec4 = img_2.sample(*sampler, v2);
+
+    *output = r1 + r2;
 }
