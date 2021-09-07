c875d330d42bb7457cffa0309c18b750ea6d44bc
eddyb <edy.burt@gmail.com>
2021-09-07 09:31:49 +0000

    Deploying to gh-pages from  @ 6807c5973d911a1359ccf88852988ce27a9df7b9 🚀

---
+++ api/implementors/std/os/fd/raw/trait.AsRawFd.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["tempfile"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/os/fd/raw/trait.AsRawFd.html\" title=\"trait std::os::fd::raw::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"tempfile/struct.NamedTempFile.html\" title=\"struct tempfile::NamedTempFile\">NamedTempFile</a>","synthetic":false,"types":["tempfile::file::NamedTempFile"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
