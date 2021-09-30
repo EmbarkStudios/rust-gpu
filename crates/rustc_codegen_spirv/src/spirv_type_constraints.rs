c63f1327ce711be42b205aa2f6876edb5cef7101
Ashley <ashley.ruglys@gmail.com>
2021-09-30 20:48:55 +0200

    Update crates/rustc_codegen_spirv/src/spirv_type_constraints.rs
    Co-authored-by: Ashley Hauck <953151+khyperia@users.noreply.github.com>

--- crates/rustc_codegen_spirv/src/spirv_type_constraints.rs
+++ crates/rustc_codegen_spirv/src/spirv_type_constraints.rs
@@ -745,7 +745,10 @@ pub fn instruction_signatures(op: Op) -> Option<&'static [InstSig<'static>]> {
         // SPV_AMD_shader_fragment_mask
         Op::FragmentMaskFetchAMD | Op::FragmentFetchAMD => reserved!(SPV_AMD_shader_fragment_mask),
         // SPV_KHR_shader_clock
-        Op::ReadClockKHR => {}
+        Op::ReadClockKHR => {
+            // NOTE(eddyb) we actually use these despite not being in the standard yet.
+            // reserved!(SPV_KHR_shader_clock)
+        }
         // SPV_NV_mesh_shader
         Op::WritePackedPrimitiveIndices4x8NV => reserved!(SPV_NV_mesh_shader),
         // SPV_NV_ray_tracing
