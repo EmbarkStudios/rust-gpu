fa69655a10c18776325234cc77d10c7277ded565
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-10-02 14:19:57 +0200

    Format the compiletest, but manually because running cargo fmt doesn't seem to do anything :shrug:

--- tests/ui/image/sample_lod.rs
+++ tests/ui/image/sample_lod.rs
@@ -1,7 +1,7 @@
 // Test `OpImageSampleExplicitLod` Lod
 // build-pass
 
-use spirv_std::{arch, Image, Sampler, image::SampledImage};
+use spirv_std::{arch, image::SampledImage, Image, Sampler};
 
 #[spirv(fragment)]
 pub fn main(
@@ -17,8 +17,6 @@ pub fn main(
     let r1: glam::Vec4 = image2d.sample_by_lod(*sampler, v2, 0.0);
     let r2: glam::Vec4 = image2d_array.sample_by_lod(*sampler, v3, 0.0);
     let r3: glam::Vec4 = cubemap.sample_by_lod(*sampler, v3, 0.0);
-    let r4: glam::Vec4 = unsafe {
-        sampled_image.sample_by_lod(v2, 0.0)
-    };
+    let r4: glam::Vec4 = unsafe { sampled_image.sample_by_lod(v2, 0.0) };
     *output = r1 + r2 + r3 + r4;
 }
