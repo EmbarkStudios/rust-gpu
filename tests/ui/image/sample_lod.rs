4e5f347239c05b4c1b9afed9da92ab3963d69501
Ashley <ashley.ruglys@gmail.com>
2021-10-08 10:33:00 +0200

    Add `SampledImage::sample_by_lod` (#755)
    * Add 'SampledImage::sample_by_lod'
    
    * Add a compiletest
    
    * Format the compiletest, but manually because running cargo fmt doesn't seem to do anything :shrug:
    
    * Run rustfmt

--- tests/ui/image/sample_lod.rs
+++ tests/ui/image/sample_lod.rs
@@ -1,7 +1,7 @@
 // Test `OpImageSampleExplicitLod` Lod
 // build-pass
 
-use spirv_std::{arch, Image, Sampler};
+use spirv_std::{arch, image::SampledImage, Image, Sampler};
 
 #[spirv(fragment)]
 pub fn main(
@@ -9,6 +9,9 @@ pub fn main(
     #[spirv(descriptor_set = 1, binding = 1)] image2d_array: &Image!(2D, type=f32, arrayed, sampled),
     #[spirv(descriptor_set = 2, binding = 2)] cubemap: &Image!(3D, type=f32, sampled),
     #[spirv(descriptor_set = 3, binding = 3)] sampler: &Sampler,
+    #[spirv(descriptor_set = 4, binding = 4)] sampled_image: &SampledImage<
+        Image!(2D, type=f32, sampled),
+    >,
     output: &mut glam::Vec4,
 ) {
     let v2 = glam::Vec2::new(0.0, 1.0);
@@ -16,5 +19,6 @@ pub fn main(
     let r1: glam::Vec4 = image2d.sample_by_lod(*sampler, v2, 0.0);
     let r2: glam::Vec4 = image2d_array.sample_by_lod(*sampler, v3, 0.0);
     let r3: glam::Vec4 = cubemap.sample_by_lod(*sampler, v3, 0.0);
-    *output = r1 + r2 + r3;
+    let r4: glam::Vec4 = unsafe { sampled_image.sample_by_lod(v2, 0.0) };
+    *output = r1 + r2 + r3 + r4;
 }
