d0a8ecaa52c0ced807708f935a67f6d7e6560db2
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2021-10-12 15:58:53 +0300

    tests: adjust for lack of `PassMode::{Indirect, Cast}`.

---
+++ tests/ui/dis/entry-pass-mode-cast-array.rs
@@ -0,0 +1,16 @@
+// This is a similar setup to the `issue-731` test, but instead of "just" the
+// missing copy out of the global (`Input`) `OpVariable`, small enough types
+// would fail much earlier (by generating unsupported pointer casts).
+// (Just like `issue-373`, the problem was the use of `PassMode::Cast`, through
+// the default Rust ABI adjustments, that we now override through query hooks)
+
+// build-pass
+// compile-flags: -C llvm-args=--disassemble-entry=main
+
+use spirv_std as _;
+
+#[spirv(fragment)]
+pub fn main(mut in_array: [f32; 2], out_array: &mut [f32; 2]) {
+    in_array[0] += 1.0;
+    *out_array = in_array;
+}
