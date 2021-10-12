28313a2029aec5de7283e167ea2d67d2aa350f62
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2021-10-12 15:58:53 +0300

    tests: adjust for lack of `PassMode::{Indirect, Cast}`.

---
+++ tests/ui/dis/issue-731.rs
@@ -0,0 +1,14 @@
+// Test that non-immediate (i.e. not one of scalar/scalar-pair/vector) inputs
+// get properly copied out of the global (`Input`) `OpVariable` and mutation is
+// only ever done on `fn`-local `OpVariable`s, not on the original global.
+
+// build-pass
+// compile-flags: -C llvm-args=--disassemble-entry=main
+
+use spirv_std as _;
+
+#[spirv(fragment)]
+pub fn main(mut in_array: [f32; 3], out_array: &mut [f32; 3]) {
+    in_array[0] += 1.0;
+    *out_array = in_array;
+}
