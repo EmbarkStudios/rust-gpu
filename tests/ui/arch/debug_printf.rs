564165f5123c41c52290628ad0d54f0603501fa3
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-10-18 04:20:27 -0700

    Add tests for function and method calls

--- tests/ui/arch/debug_printf.rs
+++ tests/ui/arch/debug_printf.rs
@@ -6,6 +6,21 @@ use spirv_std::{
     macros::{debug_printf, debug_printfln},
 };
 
+
+fn func(a: f32, b: f32) -> f32 {
+    a * b + 1.0
+}
+
+struct Struct {
+    a: f32
+}
+
+impl Struct {
+    fn method(&self, b: f32, c: f32) -> f32 {
+        self.a * b + c
+    }
+}
+
 #[spirv(fragment)]
 pub fn main() {
     unsafe {
@@ -24,5 +39,7 @@ pub fn main() {
         debug_printfln!("%v2f", vec * 3.0);
         debug_printfln!("%% %v2f %%", vec * 4.0);
         debug_printfln!("%u %i %f 🐉", 11_u32, -11_i32, 11.0_f32);
+        debug_printfln!("%f", func(33.0, 44.0));
+        debug_printfln!("%f", Struct { a: 33.0 }.method(44.0, 55.0));
     }
 }
