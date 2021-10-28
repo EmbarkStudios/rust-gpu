d45f9da016f3c869d8bbf1e9705590752a424580
Ashley Hauck <953151+khyperia@users.noreply.github.com>
2021-10-28 09:53:27 +0200

    Add test for issue #330 (#779)

---
+++ tests/ui/image/issue-330.rs
@@ -0,0 +1,18 @@
+use spirv_std::glam::Vec4;
+use spirv_std::{image::Image2dArray, Sampler};
+
+#[allow(unused_attributes)]
+#[spirv(fragment)]
+pub fn ps_main_stereo(
+    output: &mut Vec4,
+    #[spirv(descriptor_set = 0, binding = 0)] in_texture: &Image2dArray,
+) {
+    let mut color = Vec4::splat(0.0);
+
+    let mut n = 0;
+    while n < 10 {
+        let x = *in_texture;
+    }
+
+    *output = color;
+}
