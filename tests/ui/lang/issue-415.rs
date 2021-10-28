8fb4f1099501ebe9bb7d51d66a364b12747b6fc5
Ashley Hauck <953151+khyperia@users.noreply.github.com>
2021-10-28 10:13:00 +0200

    Don't ICE on ZST union (#780)

---
+++ tests/ui/lang/issue-415.rs
@@ -0,0 +1,13 @@
+// Test that zero sized unions don't ICE (even if unions are generally not supported yet)
+// build-pass
+
+use spirv_std as _;
+
+union U {
+    a: (),
+}
+
+#[spirv(fragment)]
+pub fn main() {
+    let _u = U { a: () };
+}
