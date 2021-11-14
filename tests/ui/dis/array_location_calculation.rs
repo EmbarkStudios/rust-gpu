d3521a5a88c3e8d85016c542440b1f48dcc7ecee
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-11-14 14:37:00 +0100

    Normalise the stderr

--- tests/ui/dis/array_location_calculation.rs
+++ tests/ui/dis/array_location_calculation.rs
@@ -1,5 +1,8 @@
 // build-pass
 // compile-flags: -C llvm-args=--disassemble-globals
+// normalize-stderr-test "OpCapability VulkanMemoryModel\n" -> ""
+// normalize-stderr-test "OpExtension .SPV_KHR_vulkan_memory_model.\n" -> ""
+// normalize-stderr-test "OpMemoryModel Logical Vulkan" -> "OpMemoryModel Logical Simple"
 
 use spirv_std::{
     self as _,
