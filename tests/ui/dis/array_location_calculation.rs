605f16668454ccd88f2a76f47057d864ba1bcbce
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-11-14 13:02:02 +0100

    Properly run test

--- tests/ui/dis/array_location_calculation.rs
+++ tests/ui/dis/array_location_calculation.rs
@@ -1,7 +1,7 @@
 // build-pass
-// compile-flags: -C llvm-args=--disassemble-fn=add_two_ints::add_two_ints
+// compile-flags: -C llvm-args=--disassemble-globals
 
 use spirv_std as _;
 
 #[spirv(fragment)]
-pub fn array_locations(one: [f32; 7], two: [f32; 3], three: f32) {}
+pub fn main(one: [f32; 7], two: [f32; 3], three: f32) {}
