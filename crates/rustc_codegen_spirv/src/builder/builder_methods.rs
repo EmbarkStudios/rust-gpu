0ac44083ba00149d6b5ae68b1f273e7f5912b915
khyperia <953151+khyperia@users.noreply.github.com>
2021-12-15 14:41:57 +0100

    :sweat_smile:

--- crates/rustc_codegen_spirv/src/builder/builder_methods.rs
+++ crates/rustc_codegen_spirv/src/builder/builder_methods.rs
@@ -927,16 +927,14 @@ impl<'a, 'tcx> BuilderMethods<'a, 'tcx> for Builder<'a, 'tcx> {
             let b_offset = a.value.size(self).align_to(b.value.align(self).abi);
 
             let pair_ty = place.layout.spirv_type(self.span(), self);
-            let mut load = |i, _: &Scalar, align| {
+            let mut load = |i, scalar: &Scalar, align| {
                 let llptr = self.struct_gep(pair_ty, place.llval, i as u64);
-                // WARN! This should go through to_immediate, but because we only have a Scalar,
-                // not a Ty, we can't call to_immediate here (even though we implement it as a
-                // no-op), so "inline" the no-op here and do nothing.
-                self.load(
+                let load = self.load(
                     self.scalar_pair_element_backend_type(place.layout, i, false),
                     llptr,
                     align,
-                )
+                );
+                self.to_immediate_scalar(load, *scalar)
             };
 
             OperandValue::Pair(
