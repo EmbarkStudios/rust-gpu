685c79a9725f9fb68ce17dd00e742b98a03f3870
Johan Andersson <repi@repi.se>
2022-04-19 13:47:37 +0200

    Upgrade SPIRV-Tools prebuild to 2022-04-14 (#867)
    For Linux and Windows, Mac CI uses latest installed from brew.
    Also added comment about how/where to get the download link for next upgrade.
    
    Resolves: #866

--- tests/ui/image/sample_depth_reference/sample_lod.rs
+++ tests/ui/image/sample_depth_reference/sample_lod.rs
@@ -8,12 +8,30 @@ pub fn main(
     #[spirv(descriptor_set = 0, binding = 0)] image: &Image!(2D, type=f32, sampled),
     #[spirv(descriptor_set = 1, binding = 1)] image_array: &Image!(2D, type=f32, arrayed, sampled),
     #[spirv(descriptor_set = 2, binding = 2)] sampler: &Sampler,
-    #[spirv(descriptor_set = 3, binding = 3)] cubemap: &Image!(3D, type=f32, sampled),
     output: &mut f32,
 ) {
     let v2 = glam::Vec2::new(0.0, 1.0);
     let v3 = glam::Vec3A::new(0.0, 0.0, 1.0);
     *output = image.sample_depth_reference_by_lod(*sampler, v2, 1.0, 0.0);
     *output += image_array.sample_depth_reference_by_lod(*sampler, v3, 1.0, 0.0);
+}
+
+// NOTE(eddyb) this is separate because it runs afoul of this rule:
+// > VUID-StandaloneSpirv-OpImage-04777
+// > `OpImage*Dref*` instructions **must** not consume an image whose `Dim` is 3D
+// FIXME(eddyb) look into whether non-Vulkan `OpImage*Dref*` usage can ever be 3D
+#[cfg(not(any(
+    target_env = "vulkan1.0",
+    target_env = "vulkan1.1",
+    target_env = "vulkan1.1spv1.4",
+    target_env = "vulkan1.2"
+)))]
+#[spirv(fragment)]
+pub fn main_cubemap(
+    #[spirv(descriptor_set = 2, binding = 2)] sampler: &Sampler,
+    #[spirv(descriptor_set = 3, binding = 3)] cubemap: &Image!(3D, type=f32, sampled),
+    output: &mut f32,
+) {
+    let v3 = glam::Vec3A::new(0.0, 0.0, 1.0);
     *output += cubemap.sample_depth_reference_by_lod(*sampler, v3, 1.0, 0.0);
 }
