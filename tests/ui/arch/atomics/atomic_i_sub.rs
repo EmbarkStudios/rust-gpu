b4ce5ba84894275e62e999d1d65dc77fdac61c65
wesselfr <wessel.frijters@gmail.com>
2022-08-11 11:01:51 +0200

    Added unit tests for atomic operations

---
+++ tests/ui/arch/atomics/atomic_i_sub.rs
@@ -0,0 +1,19 @@
+// build-pass
+
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
+use spirv_std::{
+    arch::IndexUnchecked,
+    memory::{Scope, Semantics},
+};
+
+#[spirv(compute(threads(64)))]
+pub fn main(#[spirv(descriptor_set = 0, binding = 0, storage_buffer)] buffer: &mut [u32]) {
+    let old = unsafe {
+        spirv_std::arch::atomic_i_sub::<
+            _,
+            { Scope::Workgroup as u32 },
+            { Semantics::UNIFORM_MEMORY.bits() },
+        >(&mut *buffer.index_unchecked_mut(0), 5)
+    };
+}
