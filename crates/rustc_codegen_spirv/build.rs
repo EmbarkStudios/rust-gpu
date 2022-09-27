b38bd60a334475540019a10d72a13fff9e777275
Sylvester Hesp <github@oisyn.nl>
2022-09-27 13:49:40 +0200

    Update to nightly 2022-09-06

--- crates/rustc_codegen_spirv/build.rs
+++ crates/rustc_codegen_spirv/build.rs
@@ -10,9 +10,9 @@ use std::process::{Command, ExitCode};
 /// `cargo publish`. We need to figure out a way to do this properly, but let's hardcode it for now :/
 //const REQUIRED_RUST_TOOLCHAIN: &str = include_str!("../../rust-toolchain");
 const REQUIRED_RUST_TOOLCHAIN: &str = r#"[toolchain]
-channel = "nightly-2022-08-29"
+channel = "nightly-2022-09-06"
 components = ["rust-src", "rustc-dev", "llvm-tools-preview"]
-# commit_hash = ce36e88256f09078519f8bc6b21e4dc88f88f523"#;
+# commit_hash = b44197abb0b3ffe4908892e1e08ab1cd721ff3b9"#;
 
 fn get_rustc_commit_hash() -> Result<String, Box<dyn Error>> {
     let rustc = std::env::var("RUSTC").unwrap_or_else(|_| String::from("rustc"));
