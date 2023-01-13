0f9cd391ce0616b931273d749c988c47ffdadfda
Sylvester Hesp <github@oisyn.nl>
2023-01-13 21:40:41 +0100

    Removed return type inference from `Image` API
    Return types are now always `glam` vectors. This also means that `glam` is required. The "glam" feature toggle is made mandatory, we may want to support other specific vector libraries in the future.

--- crates/spirv-std/src/image/params.rs
+++ crates/spirv-std/src/image/params.rs
@@ -2,60 +2,85 @@ use super::{Arrayed, Dimensionality, ImageFormat};
 use crate::{integer::Integer, scalar::Scalar, vector::Vector};
 
 /// Marker trait for arguments that accept single scalar values or vectors
-/// of scalars.
-pub trait SampleType<const FORMAT: u32>: Scalar {}
+/// of scalars. Defines 2-, 3- and 4-component vector types based on the sample type.
+pub trait SampleType<const FORMAT: u32>: Scalar {
+    /// A 2-component vector of this sample type
+    type Vec2: Default;
 
-impl SampleType<{ ImageFormat::Unknown as u32 }> for i8 {}
-impl SampleType<{ ImageFormat::Unknown as u32 }> for i16 {}
-impl SampleType<{ ImageFormat::Unknown as u32 }> for i32 {}
-impl SampleType<{ ImageFormat::Unknown as u32 }> for i64 {}
-impl SampleType<{ ImageFormat::Unknown as u32 }> for u8 {}
-impl SampleType<{ ImageFormat::Unknown as u32 }> for u16 {}
-impl SampleType<{ ImageFormat::Unknown as u32 }> for u32 {}
-impl SampleType<{ ImageFormat::Unknown as u32 }> for u64 {}
-impl SampleType<{ ImageFormat::Unknown as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Unknown as u32 }> for f64 {}
-impl SampleType<{ ImageFormat::Rgba32f as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rgba16f as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::R32f as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rgba8 as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rgba8Snorm as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rg32f as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rg16f as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::R11fG11fB10f as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::R16f as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rgba16 as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rgb10A2 as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rg16 as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rg8 as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::R16 as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::R8 as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rgba16Snorm as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rg16Snorm as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rg8Snorm as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::R16Snorm as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::R8Snorm as u32 }> for f32 {}
-impl SampleType<{ ImageFormat::Rgba32i as u32 }> for i32 {}
-impl SampleType<{ ImageFormat::Rgba16i as u32 }> for i32 {}
-impl SampleType<{ ImageFormat::Rgba8i as u32 }> for i32 {}
-impl SampleType<{ ImageFormat::R32i as u32 }> for i32 {}
-impl SampleType<{ ImageFormat::Rg32i as u32 }> for i32 {}
-impl SampleType<{ ImageFormat::Rg16i as u32 }> for i32 {}
-impl SampleType<{ ImageFormat::Rg8i as u32 }> for i32 {}
-impl SampleType<{ ImageFormat::R16i as u32 }> for i32 {}
-impl SampleType<{ ImageFormat::R8i as u32 }> for i32 {}
-impl SampleType<{ ImageFormat::Rgba32ui as u32 }> for u32 {}
-impl SampleType<{ ImageFormat::Rgba16ui as u32 }> for u32 {}
-impl SampleType<{ ImageFormat::Rgba8ui as u32 }> for u32 {}
-impl SampleType<{ ImageFormat::R32ui as u32 }> for u32 {}
-impl SampleType<{ ImageFormat::Rgb10A2ui as u32 }> for u32 {}
-impl SampleType<{ ImageFormat::Rg32ui as u32 }> for u32 {}
-impl SampleType<{ ImageFormat::Rg16ui as u32 }> for u32 {}
-impl SampleType<{ ImageFormat::Rg8ui as u32 }> for u32 {}
-impl SampleType<{ ImageFormat::R16ui as u32 }> for u32 {}
-impl SampleType<{ ImageFormat::R8ui as u32 }> for u32 {}
-impl SampleType<{ ImageFormat::R64ui as u32 }> for u64 {}
-impl SampleType<{ ImageFormat::R64i as u32 }> for i64 {}
+    /// A 3-component vector of this sample type
+    type Vec3: Default;
+
+    /// A 4-component vector of this sample type
+    type Vec4: Default;
+}
+
+/// Helper macro to implement `SampleType` of various formats for various scalar types.
+macro_rules! sampletype_impls {
+    ($($fmt:ident : $s:ty => ($v2:ty, $v3:ty, $v4:ty)),+ $(,)?) => {
+        $(
+            impl SampleType<{ ImageFormat::$fmt as u32 }> for $s {
+                type Vec2 = $v2;
+                type Vec3 = $v3;
+                type Vec4 = $v4;
+            }
+        )+
+    }
+}
+
+#[cfg(feature = "glam")]
+sampletype_impls! {
+    Unknown: i8 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    Unknown: i16 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    Unknown: i32 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    Unknown: i64 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    Unknown: u8 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    Unknown: u16 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    Unknown: u32 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    Unknown: u64 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    Unknown: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Unknown: f64 => (glam::DVec2, glam::DVec3, glam::DVec4),
+    Rgba32f: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rgba16f: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    R32f: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rgba8: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rgba8Snorm: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rg32f: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rg16f: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    R11fG11fB10f: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    R16f: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rgba16: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rgb10A2: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rg16: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rg8: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    R16: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    R8: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rgba16Snorm: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rg16Snorm: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rg8Snorm: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    R16Snorm: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    R8Snorm: f32 => (glam::Vec2, glam::Vec3, glam::Vec4),
+    Rgba32i: i32 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    Rgba16i: i32 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    Rgba8i: i32 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    R32i: i32 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    Rg32i: i32 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    Rg16i: i32 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    Rg8i: i32 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    R16i: i32 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    R8i: i32 => (glam::IVec2, glam::IVec3, glam::IVec4),
+    Rgba32ui: u32 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    Rgba16ui: u32 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    Rgba8ui: u32 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    R32ui: u32 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    Rgb10A2ui: u32 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    Rg32ui: u32 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    Rg16ui: u32 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    Rg8ui: u32 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    R16ui: u32 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    R8ui: u32 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    R64ui: u64 => (glam::UVec2, glam::UVec3, glam::UVec4),
+    R64i: i64 => (glam::IVec2, glam::IVec3, glam::IVec4),
+}
 
 /// Marker trait for arguments that accept a coordinate for an [`crate::Image`].
 pub trait ImageCoordinate<T, const DIM: u32, const ARRAYED: u32> {}
