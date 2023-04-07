4fdd48d6ef6b7eec2f26992befd9eebe7d165b2d
BeastLe9enD <beastle9end@outlook.de>
2023-04-07 03:59:48 +0200

    Add vector or scalar traits

--- crates/spirv-std/src/number.rs
+++ crates/spirv-std/src/number.rs
@@ -3,13 +3,31 @@
 /// Abstract trait representing a SPIR-V integer or floating-point type.
 pub trait Number: crate::scalar::Scalar {}
 
-impl Number for u8 {}
-impl Number for u16 {}
-impl Number for u32 {}
-impl Number for u64 {}
-impl Number for i8 {}
-impl Number for i16 {}
-impl Number for i32 {}
-impl Number for i64 {}
-impl Number for f32 {}
-impl Number for f64 {}
+/// Abstract trait representing a SPIR-V integer or floating-point type. It may also be a vector type.
+pub trait NumberVector: crate::scalar::ScalarVector {}
+
+macro_rules! impl_numbers {
+    (unsafe impl Number for $($typ:ty)*;) => {
+        $(
+            impl Number for $typ {}
+            impl NumberVector for $typ {}
+        )*
+    };
+
+    (unsafe impl NumberVector for $($typ:ty)*;) => {
+        $(
+            impl NumberVector for $typ {}
+        )*
+    };
+
+    ($(unsafe impl $trait:ident for $($typ:ty)*;)+) => {
+        $(impl_numbers!(unsafe impl $trait for $($typ)*;);)+
+    };
+}
+
+impl_numbers! {
+    unsafe impl Number for u8 u16 u32 u64 i8 i16 i32 i64 f32 f64;
+    unsafe impl NumberVector for glam::Vec2 glam::DVec2 glam::UVec2 glam::IVec2;
+    unsafe impl NumberVector for glam::Vec3 glam::DVec3 glam::UVec3 glam::IVec3;
+    unsafe impl NumberVector for glam::Vec4 glam::DVec4 glam::UVec4 glam::IVec4;
+}
--- crates/spirv-std/src/scalar.rs
+++ crates/spirv-std/src/scalar.rs
@@ -7,14 +7,47 @@
 /// not be done.
 pub unsafe trait Scalar: Copy + Default + crate::sealed::Sealed {}
 
-unsafe impl Scalar for bool {}
-unsafe impl Scalar for f32 {}
-unsafe impl Scalar for f64 {}
-unsafe impl Scalar for u8 {}
-unsafe impl Scalar for u16 {}
-unsafe impl Scalar for u32 {}
-unsafe impl Scalar for u64 {}
-unsafe impl Scalar for i8 {}
-unsafe impl Scalar for i16 {}
-unsafe impl Scalar for i32 {}
-unsafe impl Scalar for i64 {}
+/// Abstract trait representing a SPIR-V scalar type. It may also be a vector type.
+///
+/// # Safety
+/// Implementing this trait on non-scalar types breaks assumptions of other unsafe code, and should
+/// not be done.
+pub unsafe trait ScalarVector: Copy + Default {
+    /// Dimension of the vector, 1 if it is a scalar
+    const DIM: usize;
+}
+
+macro_rules! impl_scalars {
+    (unsafe impl Scalar for $($typ:ty)*;) => {
+        $(
+            unsafe impl Scalar for $typ {}
+            unsafe impl ScalarVector for $typ {
+                const DIM: usize = 1;
+            }
+        )*
+    };
+
+    (unsafe impl ScalarVector, $num: literal for $($typ:ty)*;) => {
+        $(
+            unsafe impl ScalarVector for $typ {
+                const DIM: usize = $num;
+            }
+        )*
+    };
+}
+
+impl_scalars! {
+    unsafe impl Scalar for bool f32 f64 u8 u16 u32 u64 i8 i16 i32 i64;
+}
+
+impl_scalars! {
+    unsafe impl ScalarVector, 2 for glam::BVec2 glam::Vec2 glam::DVec2 glam::UVec2 glam::IVec2;
+}
+
+impl_scalars! {
+    unsafe impl ScalarVector, 3 for glam::BVec3 glam::Vec3 glam::DVec3 glam::UVec3 glam::IVec3;
+}
+
+impl_scalars! {
+    unsafe impl ScalarVector, 4 for glam::BVec4 glam::Vec4 glam::DVec4 glam::UVec4 glam::IVec4;
+}
