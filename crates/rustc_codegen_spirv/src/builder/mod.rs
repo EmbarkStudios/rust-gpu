8637102791ab96b78b93c8789681463e8403c428
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2023-11-21 17:20:43 +0200

    Address new rustc/clippy lints.

--- crates/rustc_codegen_spirv/src/builder/mod.rs
+++ crates/rustc_codegen_spirv/src/builder/mod.rs
@@ -298,7 +298,7 @@ impl<'a, 'tcx> ArgAbiMethods<'tcx> for Builder<'a, 'tcx> {
         idx: &mut usize,
         dst: PlaceRef<'tcx, Self::Value>,
     ) {
-        fn next(bx: &mut Builder<'_, '_>, idx: &mut usize) -> SpirvValue {
+        fn next(bx: &Builder<'_, '_>, idx: &mut usize) -> SpirvValue {
             let val = bx.function_parameter_values.borrow()[&bx.current_fn.def(bx)][*idx];
             *idx += 1;
             val
