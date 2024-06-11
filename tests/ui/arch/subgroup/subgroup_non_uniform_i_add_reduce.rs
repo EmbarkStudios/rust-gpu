0cb4b38272d338c7e217f87b2f70b6f73a408f1d
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2024-06-11 13:46:43 +0200

    subgroup: added tests for group-op instructions

---
+++ tests/ui/arch/subgroup/subgroup_non_uniform_i_add_reduce.rs
@@ -0,0 +1,18 @@
+// build-pass
+// compile-flags: -C target-feature=+GroupNonUniform,+GroupNonUniformArithmetic,+ext:SPV_KHR_vulkan_memory_model
+// compile-flags: -C llvm-args=--disassemble-fn=subgroup_non_uniform_i_add_reduce::subgroup_non_uniform_i_add_reduce
+
+use glam::UVec3;
+use spirv_std::arch::{GroupOperation, SubgroupMask};
+use spirv_std::spirv;
+
+unsafe fn subgroup_non_uniform_i_add_reduce(value: u32) -> u32 {
+    spirv_std::arch::subgroup_non_uniform_i_add::<{ GroupOperation::Reduce as u32 }, _>(value)
+}
+
+#[spirv(compute(threads(32, 1, 1)))]
+pub fn main(#[spirv(local_invocation_id)] local_invocation_id: UVec3) {
+    unsafe {
+        subgroup_non_uniform_i_add_reduce(local_invocation_id.x);
+    }
+}
